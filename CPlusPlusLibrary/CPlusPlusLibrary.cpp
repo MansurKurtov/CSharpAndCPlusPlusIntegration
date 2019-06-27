6f545a10ba957883db007f409f903c58de431b5c
Mansur <Mansur@DESKTOP-6J84VFU>
2019-06-27 16:05:37 +0500

    Add project files.

---
+++ CPlusPlusLibrary/CPlusPlusLibrary.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include <iostream>
+
+typedef struct _wfs_cim_note_type
+{
+	USHORT usNoteID;
+	CHAR cCurrencyID[3];
+	ULONG ulValues;
+	USHORT usRelease;
+	BOOL bConfigured;
+} WFSCIMNOTETYPE, *LPWFSCIMNOTETYPE;
+
+typedef struct _wfs_cim_note_type_list
+{
+	USHORT usNumOfNoteTypes;
+	LPWFSCIMNOTETYPE *lppNoteTypes;
+} WFSCIMNOTETYPELIST;
+
+extern "C" __declspec(dllexport) WFSCIMNOTETYPELIST* __stdcall getCurrencyList() {
+	WFSCIMNOTETYPELIST list;
+	list.lppNoteTypes = nullptr;
+	list.usNumOfNoteTypes = 5;
+
+	WFSCIMNOTETYPELIST* address = &list;
+	std::cout << &list << std::endl;
+
+	return address;
+}
---
+++ CSharpConsole/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CSharpConsole
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var data = TestLibraryApi.getCurrencyList();
+
+            Console.WriteLine();
+            Console.WriteLine("00{0:X}", data.ToInt32());
+
+            var obj = Marshal.PtrToStructure(data, typeof(WfsCimNoteTypeList));
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public unsafe static class TestLibraryApi
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("CPlusPlusLibrary.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr getCurrencyList();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1, CharSet = CharSet.Ansi)]
+    public unsafe struct WfsCimNoteTypeList
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public ushort usNumOfNoteTypes;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [MarshalAs(UnmanagedType.ByValArray)]
+        public WfsCimNoteType[] lppNoteTypes;
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1, CharSet = CharSet.Ansi)]
+    public unsafe struct WfsCimNoteType
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public ushort usNoteID;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public char[] cCurrencyID;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public uint ulValues;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ushort usRelease;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public byte bConfigured;
+    }
+}
